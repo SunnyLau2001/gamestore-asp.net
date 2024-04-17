dad999965bd9f12caca1251efec5501fce44aa4e
sunnylau2001 <sunnylau2001fw@gmail.com>
2024-04-17 16:38:08 +0800

    Add remarks

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -6,6 +6,9 @@ public static class DataExtensions
 {
     public static void MigrateDb(this WebApplication app)
     {
+        // Scope serve for the same request.
+        // If new request, a new instance will be created.
+        // NOT A SINGLETON
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
 
