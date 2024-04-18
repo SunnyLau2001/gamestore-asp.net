ebd60b93372e6d8ab3f8ffb132134eab4c2082f6
sunnylau2001 <sunnylau2001fw@gmail.com>
2024-04-19 00:53:46 +0800

    Add client to handle games data; add table to display game summary

---
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -0,0 +1,34 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games =
+    [
+        new() {
+            Id = 1,
+            Name = "Street Fighter VI",
+            Genre = "Fighting",
+            Price = 39.59M,
+            ReleaseDate = new DateOnly(2023, 6, 2)
+        },
+        new() {
+            Id = 2,
+            Name = "Final Fantasy 14",
+            Genre = "Roleplaying",
+            Price = 59.99M,
+            ReleaseDate = new DateOnly(2010, 9, 30)
+        },
+                new() {
+            Id = 3,
+            Name = "FIFA 23",
+            Genre = "sport",
+            Price = 69.99M,
+            ReleaseDate = new DateOnly(2023, 6, 2)
+        }
+    ];
+
+    public GameSummary[] GetGames() => [.. games];
+
+}
