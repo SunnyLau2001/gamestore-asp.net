c6b9fe9bf7e5be73d0c1d8adcff7406862d98ac9
sunnylau2001 <sunnylau2001fw@gmail.com>
2024-04-19 18:54:17 +0800

    Add validation for editGame; add GetGames in GamesClient

--- GameStore.Frontend/Models/GameDetails.cs
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -1,10 +1,17 @@
-﻿namespace GameStore.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Frontend.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+    [Required(ErrorMessage = "The Genre field is required.")]
     public string? GenreId { get; set; }
+    [Required]
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 
