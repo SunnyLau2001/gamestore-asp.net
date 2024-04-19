0c936ecf75f5273aacf150d4d26834ccdb9c7439
sunnylau2001 <sunnylau2001fw@gmail.com>
2024-04-19 18:08:37 +0800

    Add EditGame page, GenresClient

---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,37 @@
+using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres =
+    [
+        new()
+        {
+            Id = 1,
+            Name = "Fighting"
+        },
+        new()
+        {
+            Id = 2,
+            Name = "Roleplaying"
+        },
+        new()
+        {
+            Id = 3,
+            Name = "Sports"
+        },
+        new()
+        {
+            Id = 4,
+            Name = "Racing"
+        },
+        new()
+        {
+            Id = 5,
+            Name = "Kids and Family"
+        },
+    ];
+
+    public Genre[] GetGenres() => genres;
+}
---
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStore.Frontend.Models;
+
+public class GameDetails
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? GenreId { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+
+}
